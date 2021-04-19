d55c9e12ba018976ec1223cc577f12619397b5bc
GalinaSherbak84 <galinasherbak@gmail.com>
2021-04-19 22:32:08 +0200

    printare le icone colorate

--- data.js
+++ data.js
@@ -97,12 +97,18 @@ const icons = [
     },
 ];
 
+//colori
+const colors = ['#001858', '#8bd3dd', '#f582ae'];
+
 const container = document.querySelector('.icons');
 //console.log(container);
 
 // 1 printare le icone a schermo
-printIcons(icons, container);
+//printIcons(icons, container);
 
+// 2 printare le icone colorate
+const coloredIcons = colorIcons(icons, colors);
+printIcons(coloredIcons, container);
 
 
 
@@ -119,15 +125,43 @@ function printIcons(icons, container){
     let html = '';
     icons.forEach((icon) =>{
         //console.log(ikon);
-        const {family, prefix, name} = icon;
+        const {family, prefix, name, color} = icon;
         html += 
         `<div class="icon p-20">
             <i class="${family} ${prefix}${name} p-20" 
-            style="color: #333"></i>
+            style="color: ${color}"></i>
             <div class="titel">${name}</div>
         </div>
         `
     });
     // aggiunta icone al container
     container.innerHTML = html;
+}
+ 
+// 2 return color icons collection by type
+function colorIcons(icons, colors){
+    const types = getType(icons);
+    console.log(types);
+    console.log(colors);
+    // assegna un colore per tipo ad ogni icona.
+    const coloredIcons = icons.map((icon) =>{
+        const indexType = types.indexOf(icon.type);
+        //console.log(indexType);
+        return {
+            ...icon,
+            color: colors[indexType],
+        }
+    })
+    return coloredIcons;
+};
+
+// get icons type (unique)
+function getType(icons){
+    const types =[];
+    icons.forEach((icon) =>{
+        if(!types.includes(icon.type)){
+            types.push(icon.type);
+        }
+    }) 
+    return(types);
 }
